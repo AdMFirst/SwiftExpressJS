16302d8816bc35733bd2280fa7151b37a3aa752b
Aditya MP <211wadityaw112@gmail.com>
2022-10-11 13:05:07 +0700

    improve login page ui

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -19,8 +16,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// ---------- DO NOT TOUCH CODE ABOVe -------------------
+
+// menginitialiasi kelas router
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+
+// menambah object router ke app
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', loginRouter);
+
+
+
+// ---------- DO NOT TOUCH CODE BELOW -------------------
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,11 +48,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const host = process.env.HOST || "localhost";
-const port = process.env.PORT || 5000;
 
-app.listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}`);
-});
-
+// return this app
 module.exports = app;
