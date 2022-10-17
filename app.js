0557ea9b48b4a36a2023f56803d461cffa5dc04e
Aditya MP <211wadityaw112@gmail.com>
2022-10-17 14:14:49 +0700

    Menambah api login

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const orcl = require('oracledb');
 
 var app = express();
 
--- routes/login.js
+++ routes/login.js
@@ -5,4 +5,12 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'Swift Login Page' });
 });
 
+router.post('/', function(req, res) {
+  
+  var email = req.body.emailLogin;
+  var password = req.body.pwLogin;
+
+  res.send(req.body)
+});
+
 module.exports = router;
