d8a3cc3089a0b86c7884401c86bdb99e7d7332c3
Aditya MP <211wadityaw112@gmail.com>
2022-10-21 11:14:49 +0700

    create connection to pg db

--- middleware/dbConnectionPool.js
+++
@@ -1,1 +0,0 @@
-// TODO make a connection to the server
--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var pool = require('./db')
 
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Swift Login Page' });
 });
 
-router.post('/', function(req, res) {
+router.post('/', async function(req, res) {
   
   var email = req.body.emailLogin;
   var password = req.body.pwLogin;
 
-  res.send(req.body)
+  const result = await pool.query("select * from \"userBasic\" where email = ($1) and password = ($2)", [email, password])
+  if(!result.rows[0]){
+    res.render('login', { title: 'Swift Login Page' });
+    
+  } else {
+    res.render('index', {title: 'Welcome'})
+  }
 });
 
 module.exports = router;
